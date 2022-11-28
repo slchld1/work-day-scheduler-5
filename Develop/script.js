ff69fb5d9085fffb828564d8a473c4707060f82f
Minjae Cho <113135811+slchld1@users.noreply.github.com>
2022-11-28 17:03:58 -0500

    added localStorage

--- Develop/script.js
+++ Develop/script.js
@@ -3,21 +3,17 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  $('.saveBtn').on("click", function(event) {
+    var input = event.target.parentElement.children[1].value
+      var keyOfInput = event.target.parentElement.children[0].innerText
+      localStorage.setItem(keyOfInput, input)
+    });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage
-  $('.saveBtn').click(function() {
-    alert('saved')
-  });
-  $('.description').keydown((e) => {
-    if (e.keyCode == 13 && !e.shiftKey) {
-      e.preventDefault();
-      return false;
-    }
-  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -42,7 +38,6 @@ $(function () {
       //convert id into number value by replacing the 'hour-' to '' and parseInt
       if (parseInt($(timeBlock[i]).attr('id').replace('hour-','')) < currentTime) {
         $(elementID).addClass("past");
-        localStorage.clear();
       } else if (parseInt($(timeBlock[i]).attr('id').replace('hour-','')) > currentTime) {
         $(elementID).addClass("future");
       } else if (parseInt($(timeBlock[i]).attr('id').replace('hour-','')) === currentTime) {
@@ -55,5 +50,5 @@ $(function () {
   updateTime(); 
   setInterval(function() {
     updateTime();
-  },6000);
+  },10000);
 });
